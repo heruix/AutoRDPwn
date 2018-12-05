541e7b5eca4847a518f46169b3ca1cff1c1bfce2
JoelGMSec <34335312+JoelGMSec@users.noreply.github.com>
2018-12-05 12:51:42 +0100

    Update AutoBypass.ps1

--- Sources/Scripts/AutoBypass.ps1
+++ Sources/Scripts/AutoBypass.ps1
@@ -1,6 +1,22 @@
 $Host.UI.RawUI.BackgroundColor = 'Black' ; $Host.UI.RawUI.ForegroundColor = 'Black' ; $Host.PrivateData.ErrorForegroundColor = 'Red' ; $Host.PrivateData.WarningForegroundColor = 'Magenta' ; $Host.PrivateData.DebugForegroundColor = 'Yellow' ; $Host.PrivateData.VerboseForegroundColor = 'Green' ; $Host.PrivateData.ProgressForegroundColor = 'White' ; $Host.PrivateData.ProgressBackgroundColor = 'Blue'
 
 REG DELETE "HKCU\Console"/f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable00 /t REG_DWORD /d 789516 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable01 /t REG_DWORD /d 14300928 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable02 /t REG_DWORD /d 958739 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable03 /t REG_DWORD /d 14521914 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable04 /t REG_DWORD /d 2035653 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable05 /t REG_DWORD /d 9967496 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable06 /t REG_DWORD /d 40129 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable07 /t REG_DWORD /d 13421772 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable08 /t REG_DWORD /d 7763574 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable09 /t REG_DWORD /d 16742459 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable10 /t REG_DWORD /d 837142 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable11 /t REG_DWORD /d 14079585 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable12 /t REG_DWORD /d 5654759 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable13 /t REG_DWORD /d 10354868 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable14 /t REG_DWORD /d 10875385 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable15 /t REG_DWORD /d 15921906 /f 2>&1> $null
 REG ADD "HKCU\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe" /t REG_SZ /v FaceName /d Consolas /f 2>&1> $null
 REG ADD "HKCU\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe" /v FontFamily /t REG_DWORD /d 54 /f 2>&1> $null
 REG ADD "HKCU\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe" /v FontWeight /t REG_DWORD /d 700 /f 2>&1> $null
