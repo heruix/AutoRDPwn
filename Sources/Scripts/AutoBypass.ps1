f7bab5d22f82743e9810ed46deb0d39a1f6b5d52
JoelGMSec <34335312+JoelGMSec@users.noreply.github.com>
2018-12-05 15:13:38 +0100

    Update AutoBypass.ps1

--- Sources/Scripts/AutoBypass.ps1
+++ Sources/Scripts/AutoBypass.ps1
@@ -1,6 +1,8 @@
 $Host.UI.RawUI.BackgroundColor = 'Black' ; $Host.UI.RawUI.ForegroundColor = 'Black' ; $Host.PrivateData.ErrorForegroundColor = 'Red' ; $Host.PrivateData.WarningForegroundColor = 'Magenta' ; $Host.PrivateData.DebugForegroundColor = 'Yellow' ; $Host.PrivateData.VerboseForegroundColor = 'Green' ; $Host.PrivateData.ProgressForegroundColor = 'White' ; $Host.PrivateData.ProgressBackgroundColor = 'Blue'
 
 REG DELETE "HKCU\Console"/f 2>&1> $null
+REG ADD "HKCU\Console" /v ForceV2 /t REG_DWORD /d 1 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ExtendedEditKey /t REG_DWORD /d 1 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable00 /t REG_DWORD /d 789516 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable01 /t REG_DWORD /d 14300928 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable02 /t REG_DWORD /d 958739 /f 2>&1> $null
@@ -11,7 +13,7 @@ REG ADD "HKCU\Console" /v ColorTable06 /t REG_DWORD /d 40129 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable07 /t REG_DWORD /d 13421772 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable08 /t REG_DWORD /d 7763574 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable09 /t REG_DWORD /d 16742459 /f 2>&1> $null
-REG ADD "HKCU\Console" /v ColorTable10 /t REG_DWORD /d 837142 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable10 /t REG_DWORD /d 7915030 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable11 /t REG_DWORD /d 14079585 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable12 /t REG_DWORD /d 5654759 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable13 /t REG_DWORD /d 10354868 /f 2>&1> $null
