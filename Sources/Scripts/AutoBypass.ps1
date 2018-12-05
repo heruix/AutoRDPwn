293c0fee15f79f063548a253b5b91e767758468e
JoelGMSec <34335312+JoelGMSec@users.noreply.github.com>
2018-12-05 17:42:21 +0100

    Update AutoBypass.ps1

--- Sources/Scripts/AutoBypass.ps1
+++ Sources/Scripts/AutoBypass.ps1
@@ -12,7 +12,7 @@ REG ADD "HKCU\Console" /v ColorTable05 /t REG_DWORD /d 9967496 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable06 /t REG_DWORD /d 40129 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable07 /t REG_DWORD /d 13421772 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable08 /t REG_DWORD /d 7763574 /f 2>&1> $null
-REG ADD "HKCU\Console" /v ColorTable09 /t REG_DWORD /d 16752700 /f 2>&1> $null
+REG ADD "HKCU\Console" /v ColorTable09 /t REG_DWORD /d 16742459 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable10 /t REG_DWORD /d 7915030 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable11 /t REG_DWORD /d 14079585 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable12 /t REG_DWORD /d 5901010 /f 2>&1> $null
